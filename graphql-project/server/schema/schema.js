435521dedcf75508b08093379a41acd6fa2a9d3a
MaximVovk <maksemvovk@gmail.com>
2021-09-14 23:53:48 +0300

    RootQuery and Running Queries on GraphiQL

--- graphql-project/server/schema/schema.js
+++ graphql-project/server/schema/schema.js
@@ -3,16 +3,44 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLInt,
+  GraphQLSchema,
 } = require('graphql')
 
 // Create types
+
+const users = [
+  { id:'1', name: 'Bill', age:20 },
+  { id:'2', name: 'Samantha', age:21 }
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   description: 'Documentation for user',
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
   })
+})
+
+// RootQuery
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  description: 'Description',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return users.find(f => f.id === args.id)
+      },
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 })
