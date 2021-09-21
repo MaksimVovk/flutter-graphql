64a7c1bdf002e569cc5dc9401cbc9f43f144c42a
MaximVovk <maksemvovk@gmail.com>
2021-09-21 15:33:55 +0300

    add userId into mongoose hobby model

--- graphql-project/server/model/hobby.js
+++ graphql-project/server/model/hobby.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose')
 
 const MSchema = mongoose.Schema
 
-mongoose.set('useFindAndModify', false)
-
 const hobbySchema = MSchema({
   title: String,
   description: String,
+  userId: String,
 })
 
 module.exports = mongoose.model('Hobby', hobbySchema)
