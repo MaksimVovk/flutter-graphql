281d422cff1a47338fc38c9e0bc15acc72fab95a
MaximVovk <maksemvovk@gmail.com>
2021-09-28 10:03:08 +0300

    Setting up the AddUser Screen - Creating the

--- flutter-client/flutter_graphql/lib/screens/add_user_page.dart
+++ flutter-client/flutter_graphql/lib/screens/add_user_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class AddUserPage extends StatefulWidget {
   @override
@@ -6,10 +7,65 @@ class AddUserPage extends StatefulWidget {
 }
 
 class _AddUserPageState extends State<AddUserPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String insertUser () {
+    return '';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add a user',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 19,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 6),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 10),
+                color: Colors.grey.shade300,
+                blurRadius: 30,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Mutation(
+                options: MutationOptions(
+                  document: gql(insertUser()),
+                  fetchPolicy: FetchPolicy.noCache,
+                  onCompleted: (data) {
+                    
+                  },
+                ),
+                builder: (runMutation, result) {
+                  return Form(
+                    key: _formKey,
+                    child: Column(
+
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
